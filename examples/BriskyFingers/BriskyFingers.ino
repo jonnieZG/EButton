b614218619e49b6bfdd46af3dd223927e4867a28
jonnieZG <jonnieZG@users.noreply.github.com>
2017-02-18 21:14:54 +0100

    Create BriskyFingers.ino

---
+++ examples/BriskyFingers/BriskyFingers.ino
@@ -0,0 +1,19 @@
+#include "Arduino.h"
+#include "EButton.h"
+
+EButton button(2);
+
+void countClicks(EButton &btn) {
+	Serial.print("\nYou've managed to click ");
+	Serial.print(btn.getClicks());
+	Serial.println(" time(s)!");
+}
+void setup() {
+	Serial.begin(115200);
+	button.attachAnyClick(countClicks);
+	Serial.println("\nClick as fast as you can!");
+}
+
+void loop() {
+	button.tick();
+}
