bb80a98d3a6e1e3c0b42b0af46ddd81547cef7d9
jonnieZG <jonnieZG@gmail.com>
2019-07-26 09:40:23 +0200

    Fixed example "#define"s

--- examples/BasicClicks/BasicClicks.ino
+++ examples/BasicClicks/BasicClicks.ino
@@ -1,7 +1,12 @@
 #include "Arduino.h"
 
-// ------- Specify required features before including the EButton.h --------
-#define EBUTTON_SUPPORT_SINGLE_AND_DOUBLE_CLICKS
+// ------- Specify not required features before including the EButton.h --------
+#define EBUTTON_SUPPORT_TRANSITION_DISABLED
+#define EBUTTON_SUPPORT_EACH_CLICK_DISABLED
+#define EBUTTON_SUPPORT_DONE_CLICKING_DISABLED
+//#define EBUTTON_SUPPORT_SINGLE_AND_DOUBLE_CLICKS_DISABLED
+#define EBUTTON_SUPPORT_LONG_PRESS_START_DISABLED
+#define EBUTTON_SUPPORT_LONG_PRESS_DURING_DISABLED
 
 #include "EButton.h"
 
